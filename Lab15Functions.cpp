f03ae9c61a731de97ae58ed2308161eeec62ae38
Austin Hall <halaus@umich.edu>
2024-03-20 12:58:16 -0400

    Functions Built

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -1,9 +1,29 @@
+#include <iostream>
+double factorial(int);
 /*******************************************************************
 * getInfo *
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 *******************************************************************/
+
 void getInfo(int & pickFrom, int & numPicks){
+    using namespace std;
+    if(pickFrom<1 || pickFrom>12){
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin >> pickFrom;
+        if (pickFrom<1 || pickFrom>12){
+            cout<<"Input Error!"<<endl;
+            getInfo(pickFrom, numPicks);
+        }
+    }
+    if(numPicks<1 || numPicks>7){
+        cout<<"How many balls (1-7) will be drawn? ";
+        cin>>numPicks;
+        if (numPicks<1 || numPicks>7){
+            cout<<"Input Error!"<<endl;
+            getInfo(pickFrom, numPicks);
+        }
+    }
 
 }
 
@@ -19,10 +39,14 @@ void getInfo(int & pickFrom, int & numPicks){
 // multiplying two factorials together. This is done to prevent any
 // intermediate result becoming so large that it causes overflow.
 double computeWays(int n, int k){
-    
+int h = n - k;
+    double answer = factorial(k)*factorial(h)/factorial(n);
+    return answer;
 }
 
 // This function computes factorials recursively. It is called by computeWays.
 double factorial(int n){
-
+if(n==0){
+    return 1;
+} return n*factorial(n-1);
 }
